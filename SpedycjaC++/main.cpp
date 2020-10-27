f96cb33fabcda8519aad0a4b9b664114374bf056
Jan Piaskowy <piaskowyjasiek@gmail.com>
2020-10-27 18:44:48 +0100

    Initial Commit

---
+++ SpedycjaC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpedycjaC++
+//
+//  Created by Jan Piaskowy on 27/10/2020.
+//  Copyright © 2020 Jan Piaskowy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
